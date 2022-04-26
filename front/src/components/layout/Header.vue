bc4bfee759793e680afa7ccc5f8e1d3db81ffb3b
Kim Young Min <kastori1990@gmail.com>
2022-04-26 15:09:25 +0900

    게시판 리스트 구현

---
+++ front/src/components/layout/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-navbar toggleable type="dark" variant="dark">
+    <b-navbar-brand href="/">Simple_Board</b-navbar-brand>
+
+    <b-dropdown size="lg" text="User" class="m-2" right>
+      <b-dropdown-item-button>Action</b-dropdown-item-button>
+      <b-dropdown-item-button>Another action</b-dropdown-item-button>
+      <b-dropdown-item-button>Something else here</b-dropdown-item-button>
+    </b-dropdown>
+  </b-navbar>
+</template>
+<script>
+export default {
+  name: "L_Header",
+};
+</script>
+
+<style></style>
---
+++ front/src/views/board/BoardList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+  </div>
+</template>
+
+<script>
+import data from "@/data";
+
+let items = data.Content.sort((a, b) => {
+  return b.id - a.id;
+});
+items = items.map((contentItem) => {
+  return {
+    ...contentItem,
+    user_name: data.User.filter(
+      (userItem) => userItem.user_id === contentItem.member_id
+    )[0].name,
+  };
+});
+export default {
+  data() {
+    return {
+      fields: [
+        {
+          key: "id",
+          label: "글번호",
+        },
+        {
+          key: "title",
+          label: "제목",
+        },
+        {
+          key: "user_name",
+          label: "작성자",
+        },
+        {
+          key: "date",
+          label: "작성일",
+        },
+        {
+          key: "view_count",
+          label: "조회",
+        },
+      ],
+      items: items,
+    };
+  },
+};
+</script>
