e1c0ba095b2a7d3c7240a20327654f35fa897286
Kim Young Min <kastori1990@gmail.com>
2022-05-02 20:12:52 +0900

    router 로그인, 회원가입 페이지 추가

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Header from "@/layout/AppHeader";
 import Board from "@/views/board/BoardList.vue";
+import BoardDetail from "@/views/board/BoardDetail";
+import Login from "@/views/VueLogin";
+import Register from "@/views/VueRegister";
 
 Vue.use(VueRouter);
 
@@ -8,7 +12,34 @@ const routes = [
   {
     path: "/",
     name: "board",
-    component: Board,
+    components: {
+      header: Header,
+      default: Board,
+    },
+  },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    components: {
+      header: Header,
+      default: BoardDetail,
+    },
+  },
+  {
+    path: "/login",
+    name: "login",
+    components: {
+      header: Header,
+      default: Login,
+    },
+  },
+  {
+    path: "/register",
+    name: "register",
+    components: {
+      header: Header,
+      default: Register,
+    },
   },
 ];
 
